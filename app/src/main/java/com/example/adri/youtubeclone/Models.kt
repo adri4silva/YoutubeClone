b412ba406a667a6ebf79a52553d9636841acda27
adri4silva <adri4silva@gmail.com>
2018-01-10 10:20:11 +0100

    New Model Classes Added

--- app/src/main/java/com/example/adri/youtubeclone/Models.kt
+++ app/src/main/java/com/example/adri/youtubeclone/Models.kt
@@ -9,3 +9,5 @@ class HomeFeed(val videos: List<Video>)
 class Video(val id: Int, val name: String, val link: String, val imageUrl: String, val numberOfViews: Int, val channel: Channel)
 
 class Channel(val name: String, val profileImageUrl: String, val numberOfSubscribers: Int)
+
+class Lesson(val name: String, val duration: String, val number: Int, val imageUrl: String, val link: String)
